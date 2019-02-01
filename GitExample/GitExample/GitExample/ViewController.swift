6729dd1e78067ff0ed6bf713531a38d731b27262
Joseph Tamas <joseph.tamas@endava.com>
2019-02-01 13:34:24 +0200

    Created Project named GitExample

---
+++ GitExample/GitExample/GitExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitExample
+//
+//  Created by Joseph Tamas on 01/02/2019.
+//  Copyright © 2019 Joseph Tamas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
